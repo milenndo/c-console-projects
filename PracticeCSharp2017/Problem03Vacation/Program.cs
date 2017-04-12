3da1e13f2a984212dd825da0f9815c50f8a4f79b
milenndo <milenndo@gmail.com>
2017-04-13 00:03:47 +0300

    task03

--- PracticeCSharp2017/Problem03Vacation/Program.cs
+++ PracticeCSharp2017/Problem03Vacation/Program.cs
@@ -1,15 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Problem03Vacation
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            double  budget = double.Parse(Console.ReadLine());
+            
+            string season = Console.ReadLine();
+            
+            
+            
+            // bugeat < 1000 leva
+
+            if (budget <= 1000)
+            {
+                if (season == "Summer")
+                {
+                    string place = "Camp";
+                    string location = "Alaska";
+                    double price = budget * 0.65;
+                    Console.WriteLine($"{location} - {place} - {price:f2}");
+                }
+                if (season == "Winter")
+                {
+                    string place = "Camp";
+                    string location = "Morocco";
+                    double price = budget * 0.45;
+                    Console.WriteLine($"{location} - {place} - {price:f2}");
+                }
+ 
+            }
+
+            // budget > 1000<= 3000
+
+            if (budget > 1000 && budget <= 3000)
+            {
+                if (season == "Summer")
+                {
+                    string place = "Hut";
+                    string location = "Alaska";
+                    double price = budget * 0.80;
+                    Console.WriteLine($"{location} - {place} - {price:f2}");
+                }
+                if (season == "Winter")
+                {
+                    string place = "Hut";
+                    string location = "Morocco";
+                    double price = budget * 0.60;
+                    Console.WriteLine($"{location} - {place} - {price:f2}");
+                }
+
+            }
+
+            // budget > 3000;
+            if (budget >= 3000)
+            {
+                if (season == "Summer")
+                {
+                    string place = "Hotel";
+                    string location = "Alaska";
+                    double price = budget * 0.90;
+                    Console.WriteLine($"{location} - {place} - {price:f2}");
+                }
+                if (season == "Winter")
+                {
+                    string place = "Hotel";
+                    string location = "Morocco";
+                    double price = budget * 0.90;
+                    Console.WriteLine($"{location} - {place} - {price:f2}");
+                }
+
+                
+            }
+           
         }
     }
 }
