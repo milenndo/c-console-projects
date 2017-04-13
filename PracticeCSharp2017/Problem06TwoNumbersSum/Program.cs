19f95d7de51bf674de24cd98cacf4587af0413f4
milenndo <milenndo@gmail.com>
2017-04-13 03:44:07 +0300

    task06

---
+++ PracticeCSharp2017/Problem06TwoNumbersSum/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem06TwoNumbersSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startInterval = int.Parse(Console.ReadLine());
+            int finalInterval = int.Parse(Console.ReadLine());
+            int magicalNum = int.Parse(Console.ReadLine());
+            int combination = 0;
+          
+            for (int i = startInterval; i >= finalInterval; i--)
+            {
+                for (int j = startInterval; j >= finalInterval; j--)
+                {
+                    combination++;
+                    if(i + j == magicalNum)
+                    {
+                        Console.WriteLine("Combination N:{0} ({1} + {2} = {3})", combination,i,j,magicalNum);
+                        return;
+                    }
+                }
+            }
+
+            for (int i = startInterval; i >= finalInterval; finalInterval--)
+            {
+                for (int j = startInterval; j >= finalInterval; finalInterval--)
+                {
+                    combination++;
+                    if (i+j != 0)
+                    {
+                        Console.WriteLine("{0} combinations - neither equals {1}", combination - 1, magicalNum);
+                        return;
+                    }
+                }
+            }
+
+           
+
+
+            
+
+            
+            
+            
+
+
+
+        }
+    }
+}
